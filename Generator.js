9c3d32228dbc93267b4ced5e1a8a40e8170c275c
Liam <liam@aether>
2015-07-25 18:15:11 -0400

    ES5 Generators, with tests

---
+++ Generator.js
@@ -0,0 +1,344 @@
+/**
+ * Streamable promises 
+ * 
+ * These are promises which always deliver arrays, but also support returning each individual item
+ * as they are obtained in addition to all once the promise is finally resolved.
+ * They are similar to ES6 generators, but are asynchronous via callbacks instead of co-routines.
+ * 
+ * @param {type} cb
+ * @returns {Generator}
+ */
+
+function Generator(cb) {
+	var self = this;
+	
+	self._registeredEmits = [];
+	self._registeredCatches = [];
+	self._registeredDones = [];
+	
+	var onEmit = function(item) {
+		var yields = self._emitters;
+		for (var i = 0, max = yields.length; i < max; ++i) {
+			var cb = yields[i];
+			
+			cb.assign();
+		}
+	};
+	
+	var callback = function(fns, args) {
+		for (var i = 0, max = fns.length; i < max; ++i) {
+			fns[i].apply(null, args);
+		}
+	};
+	
+	var done = function() {
+		callback(self._registeredDones, []);
+	};
+	
+	var reject = function(error) {
+		callback(self._registeredCatches, [error]);
+	};
+	
+	var emit = function(item) {
+		callback(self._registeredEmits, [item]);
+	};
+	
+	// Unlike promises, callbacks to generator functions _must_ be asynchronous
+	// to ensure anyone even has a chance to register for the first item in some cases.
+	// Since this is done on a timeout, it ensures that you have until control is released
+	// from your function to register emits, dones, and thens before the first item is generated.
+	// Items are NOT stored after they are emitted, if you miss it you won't get it.
+	
+	setTimeout(function() {
+		cb(done, reject, emit);
+	}, 1);
+}
+
+// Integrate with the environment.
+
+if (typeof window !== 'undefined')
+	window.Generator = Generator;
+
+if (typeof module !== 'undefined')
+	module.exports = Generator;
+
+// Errors thrown from generators
+
+Generator.InvalidResolution = {
+	error: 'streamablePromise-invalid-resolution',
+	message: 'You cannot resolve() a Generator with a value. '+
+			 'The value of a streamable promise is always the array of yielded items'
+};
+Generator.InvalidSubpromiseResolution = {
+	error: 'streamablePromise-invalid-subresolution',
+	message: 'While attempting to combine the results of multiple promises, '+
+			 'one of the (non-streamable) promises returned an item which was not an array.'
+};
+
+// Static methods
+
+/**
+ * Union the results of the promises into a single Generator
+ * 
+ * @param {type} promises
+ * @returns {undefined}
+ */
+Generator.union = function(promises) {
+	return new Generator(function(resolve, reject, emit) {
+		for (var i = 0, max = promises.length; i < max; ++i) {
+			var promise = promises[i];
+
+			if (promise.emit) {
+				promise.emit(function(item) {
+					emit(item);
+				});
+			} else {
+				promise.then(function(items) {
+					if (typeof items !== 'object' || items.length === undefined) {
+						throw Generator.InvalidSubpromiseResolution;
+					}
+					
+					for (var j = 0, jMax = items.length; j < jMax; ++j) {
+						emit(items[j]);
+					}
+				});
+			}
+		}
+	});
+};
+
+/**
+ * Return items which occur in setA but not in setB.
+ * Equality is determined via strict equals (===).
+ * Pass a comparator function to override this behavior.
+ * 
+ * @param {type} promiseForBigSet
+ * @param {type} promiseForItemsToExclude
+ * @returns {undefined}
+ */
+Generator.exclude = function(setA, setB, comparator)
+{
+	if (comparator === undefined) {
+		comparator = function(a, b) {
+			return a === b;
+		}
+	}
+	
+	return new Generator(function(resolve, reject, emit) {
+		var itemsA = [];
+		var itemsB = [];
+
+		Promise.all([
+			setA.then(function(items) {
+				itemsA = items;
+			}),
+
+			setB.then(function(items) {
+				itemsB = items;
+			})
+		]).then(function() {
+			for (var i = 0, max = itemsA.length; i < max; ++i) {
+				var itemA = itemsA[i];
+				var skip = false;
+
+				for (var j = 0, jMax = itemsB.length; j < jMax; ++j) {
+					var itemB = itemsB[j];
+
+					if (comparator(itemA, itemB)) {
+						skip = true;
+						break;
+					}
+				}
+
+				if (skip)
+					continue;
+
+				emit(itemA);
+			}
+			
+			resolve();
+		});
+	});
+}
+
+/**
+ * Intersects the given set of streamable promises, using the given "hasher" function
+ * to produce an ID string for each object. This approach is much more efficient than intersecting
+ * by comparison (intersectByComparison()), so this should be used instead whenever possible.
+ * Efficiency: ?
+ * 
+ * @param {type} promises
+ * @param {type} hasher
+ * @returns {undefined}
+ */
+Generator.intersectByHash = function(promises, hasher) {
+	
+	return new Generator(function(resolve, reject, emit) {
+		var map = {};
+		var handlers = [];
+		
+		for (var i = 0, max = promises.length; i < max; ++i) {
+			var promise = promises[i];
+			
+			var handleEmit = function(item) {
+				var id = identify(item);
+				var count = 0;
+				if (map[id])
+					count = map[id];
+				
+				count += 1;
+				map[id] = count;
+				
+				if (count == promises.length) {
+					emit(item);
+				}
+			}
+			
+			if (promise.emit) {
+				handlers.push(new Promise(function(resolve, reject) {
+					handlers.emit(handleEmit).then(function() {
+						resolve();
+					});
+				}));
+			} else {
+				handlers.push(promise.then(function(items) {
+					for (var j = 0, jMax = items.length; j < jMax; ++j) {
+						handleEmit(items[j]);
+					}
+				}));
+			}
+		}
+		
+		Promise.all(handlers).then(function() {
+			resolve();
+		});
+	});
+}
+
+/**
+ * Intersects the given set of streamable promises, using the given "comparator" function
+ * to determine if two objects are equal. This form of intersect operation can be much 
+ * less efficient than intersection by identity (intersectByIdentity). Efficiency n^2
+ * 
+ * @param {type} promises
+ * @param {type} identify
+ * @returns {undefined}
+ */
+Generator.intersectByComparison = function(promises, comparator) {
+	
+	return new Generator(function(resolve, reject, emit) {
+		var handlers = [];
+		var distinctItems = [];
+		
+		for (var i = 0, max = promises.length; i < max; ++i) {
+			var promise = promises[i];
+			
+			var handleEmit = function(item) {
+				var found = false;
+				
+				for (var j = 0, jMax = distinctItems.length; j < jMax; ++j) {
+					var distinctItem = distinctItems[j];
+					
+					if (comparator(distinctItem.item, item)) {
+						distinctItem.count += 1;
+						
+						if (!distinctItem.emitted && distinctItem.count == promises.length) {
+							distinctItem.emitted = true;
+							emit(distinctItem.item);
+						}
+						
+						found = true;
+						break;
+					}
+				}
+				
+				if (found)
+					return;
+				
+				var distinctItem = {
+					item: item,
+					count: 1,
+					emitted: false
+				};
+				
+				if (distinctItem.count == promises.length) {
+					emit(distinctItem.item);
+					distinctItem.emitted = true;
+				}
+				
+				distinctItems.push(distinctItem);
+			}
+			
+			if (promise.emit) {
+				handlers.push(new Promise(function(resolve, reject) {
+					promise.emit(handleEmit).then(function() {
+						resolve();
+					});
+				}));
+			} else {
+				handlers.push(promise.then(function(items) {
+					for (var j = 0, jMax = items.length; j < jMax; ++j) {
+						handleEmit(items[j]);
+					}
+				}));
+			}
+		}
+		
+		Promise.all(handlers).then(function() {
+			resolve();
+		});
+	});
+}
+
+// Instance methods
+
+/**
+ * Register a callback for the emit event, when the streamable promise
+ * emits an item.
+ * 
+ * @param {type} cb
+ * @returns {Generator.prototype}
+ */
+Generator.prototype.emit = function(cb) {
+	this._registeredEmits.push(cb);
+	return this;
+};
+
+Generator.prototype.catch = function(cb) {
+	this._registeredCatches.push(cb);
+	return this;
+};
+
+Generator.prototype.done = function(cb) {
+	this._registeredDones.push(cb);
+	return this;
+};
+
+/**
+ * To blend nicely with Promises, Generators can start an actual Promise chain.
+ * NOTE that this will cause the Generator to store an array of all results emitted 
+ * between the registration of this function and when the generator complets (.done()).
+ * 
+ * This will cause O(N) memory instead of O(1), so only use this if you don't mind storing
+ * all generator results into an array (very very bad idea for an infinite set).
+ * 
+ * @param {type} cb
+ * @returns {Promise}
+ */
+Generator.prototype.then = function(cb) {
+	var self = this;
+	return new Promise(function(resolve, reject) {
+		var items = [];
+		
+		self.emit(function(item) {
+			items.push(item);
+		}).done(function() {
+			try {
+				var result = cb(items);
+				resolve(result);
+			} catch (e) {
+				reject(e);
+			}
+		});
+	});
+};
---
+++ test/GeneratorSpec.js
@@ -0,0 +1,80 @@
+
+describe("Generator", function() {
+	it("provides done, reject, emit to its generator function", function(_done) {
+		new Generator(function(done, reject, emit) {
+			expect(typeof done).toBe('function');
+			expect(typeof reject).toBe('function');
+			expect(typeof emit).toBe('function');
+			_done();
+		});
+	});
+	
+	it("calls emit listeners for each item emitted", function(_done) {
+		var generator = new Generator(function(done, reject, emit) {
+			emit(1);
+			emit(2);
+			emit(3);
+			done();
+		});
+		
+		var i = 1;
+		generator.emit(function(item) {
+			expect(item).toBe(i);
+			i += 1;
+			
+			if (i == 3)
+				_done();
+		});
+	});
+	
+	it("calls done listeners after the generator function finishes", function(_done) {
+		var generator = new Generator(function(done, reject, emit) {
+			emit(1);
+			emit(2);
+			emit(3);
+			done();
+		});
+		
+		generator.done(function() {
+			_done();
+		});
+	});
+	
+	it("calls both emit listeners and done listeners properly", function(_done) {
+		var generator = new Generator(function(done, reject, emit) {
+			emit(1);
+			emit(2);
+			emit(3);
+			done();
+		});
+		
+		var i = 1;
+		
+		generator.emit(function(item) {
+			expect(item).toBe(i);
+			i += 1;
+		}).done(function() {
+			expect(i == 4).toBe(true);
+			_done();
+		});
+	});
+	
+	it("lets you start a promise chain", function(_done) {
+		var generator = new Generator(function(done, reject, emit) {
+			emit(1);
+			emit(2);
+			emit(3);
+			done();
+		});
+		
+		var i = 1;
+		
+		generator.then(function(allResults) {
+			expect(allResults.length).toBe(3);
+			return 'foo';
+		}).then(function(shouldBeFoo) {
+			expect(shouldBeFoo).toBe('foo');
+			_done();
+		});
+	});
+});
